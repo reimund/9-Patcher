d6ac096b3ec7f1edc316b5e8b9be0cf1937eac33
Reimund Trost <reimund@code7.se>
2011-05-02 23:09:46 +0200

    Right and bottom patches can now be specified as well.

--- 9patcher.py
+++ 9patcher.py
@@ -17,13 +17,16 @@ def main(argv=None):
 
 	parser = argparse.ArgumentParser(description='Make 9 patch images.')
 	parser.add_argument('-t', metavar='start,length', nargs='+', help='black pixels in top margin.')
+	parser.add_argument('-r', metavar='start,length', nargs='+', help='black pixels in right margin.')
+	parser.add_argument('-b', metavar='start,length', nargs='+', help='black pixels in bottom margin.')
 	parser.add_argument('-l', metavar='start,length', nargs='+', help='black pixels in left margin.')
 	parser.add_argument('-i', metavar='input', required=True, help='input graphic file')
 	parser.add_argument('-o', metavar='output', help='output graphic file')
 
 	args = vars(parser.parse_args())
 
-	if (args['t'] == None and args['l'] == None):
+	if (args['t'] == None and args['r'] == None
+			and args['b'] == None and args['l'] == None):
 		print 'No patches defined. There\'s nothing for me to do.'
 	else:
 		make_nine_patch(args)
@@ -49,37 +52,51 @@ def make_nine_patch(args):
 	except IOError:
 		print ''.join(['Could not read ', infile, '.']) 
 		return
-
-	if args['t'] != None:
-		for patch in args['t']:
-			try:
-				(position,length) = patch.split(',')
-				for x in range(int(position), int(length) + 1):
-					pixels[x + 1, 0] = (0, 0, 0, 255)
-			except ValueError:
-				print 'Patch regions must be specified as integer pairs.'
-				return
-			except IndexError:
-				print ''.join(['A top patch region exceeds the image dimensions. Top patches must be within the range 0-', str(width - 1), '.'])
-				return
 	
-	if args['l'] != None:
-		for patch in args['l']:
-			try:
-				(position,length) = patch.split(',')
-				for y in range(int(position), int(length) + 1):
-					pixels[0, y + 1] = (0, 0, 0, 255)
-			except ValueError:
-				print 'Patch regions must be specified as integer pairs.'
-				return
-			except IndexError:
-				print ''.join(['A left patch region exceeds the image dimensions. Left paches must be within the range 0-', str(height - 1), '.'])
-				return
+	try:
+		draw_patches(args['t'], 'top', pixels, width, height)
+		draw_patches(args['r'], 'right', pixels, width, height)
+		draw_patches(args['b'], 'bottom', pixels, width, height)
+		draw_patches(args['l'], 'left', pixels, width, height)
+	except PatchError as e:
+		print e
+		return
 
 	try: 
 		out_im.save(outfile, 'png')
 	except IOError:
 		print 'Could not create output file.'
+
+def draw_patches(patches, edge, pixels, width, height):
+	if patches == None:
+		return
+	
+	range_end = str(width - 1) if (edge == 'top' or edge == 'bottom') else str(height - 1)
+	y = 0 if (edge == 'top') else height + 1
+	x = 0 if (edge == 'left') else width + 1
+
+	for patch in patches:
+		try:
+			(position,length) = patch.split(',')
+			for p in range(int(position), int(length) + 1):
+				error_msg = ''.join([ 'A ', edge, ' patch region exceeds the image dimensions. ', edge.title(), ' patches must be within the range 0-', range_end, '.'])
+
+				if (edge == 'top' or edge == 'bottom'):
+					if (p + 1 > width):
+						raise PatchError(error_msg)
+					pixels[p + 1, y] = (0, 0, 0, 255)
+				else:
+					if (p + 1 > height):
+						raise PatchError(error_msg)
+					pixels[x, p + 1] = (0, 0, 0, 255)
+					
+		except ValueError:
+			raise PatchError('Patch regions must be specified as integer pairs.')
+
+
+class PatchError(Exception):
+    pass
+
 
 if __name__ == "__main__":
     sys.exit(main())
